55198adbf6170834fc0f3a312d81c409158136c8
uncoyote <jeker.git@outlook.de>
2020-03-04 08:26:50 +0100

    Implemented toc sort

--- simi/modules/web/src/ch/so/agi/simi/web/screens/facadelayer/FacadeLayerEdit.java
+++ simi/modules/web/src/ch/so/agi/simi/web/screens/facadelayer/FacadeLayerEdit.java
@@ -1,11 +1,39 @@
 package ch.so.agi.simi.web.screens.facadelayer;
 
+import ch.so.agi.simi.entity.DatasetListProperties;
+import com.haulmont.cuba.gui.components.Button;
+import com.haulmont.cuba.gui.components.Table;
+import com.haulmont.cuba.gui.model.CollectionPropertyContainer;
+import com.haulmont.cuba.gui.model.DataContext;
 import com.haulmont.cuba.gui.screen.*;
 import ch.so.agi.simi.entity.FacadeLayer;
+
+import javax.inject.Inject;
 
 @UiController("simi_FacadeLayer.edit")
 @UiDescriptor("facade-layer-edit.xml")
 @EditedEntityContainer("facadeLayerDc")
 @LoadDataBeforeShow
 public class FacadeLayerEdit extends StandardEditor<FacadeLayer> {
+    @Inject
+    private CollectionPropertyContainer<DatasetListProperties> dataSetListPropertiesDc;
+    @Inject
+    private Table<DatasetListProperties> dataSetListPropertiesTable;
+
+    @Subscribe(target = Target.DATA_CONTEXT)
+    public void onPreCommit(DataContext.PreCommitEvent event) {
+        int i = 0;
+        // go through the data container items. The same can be done using getEditedEntity().getSingleActorList().
+        for (DatasetListProperties item : dataSetListPropertiesDc.getItems()) {
+            // set new value and add modified instance to the commit list
+            item.setSort(i);
+            event.getModifiedInstances().add(item);
+            i += 10;
+        }
+    }
+
+    @Subscribe("btnSortTable")
+    public void onBtnSortTableClick(Button.ClickEvent event) {
+        dataSetListPropertiesTable.sort("sort", Table.SortDirection.ASCENDING);
+    }
 }
--- simi/modules/web/src/ch/so/agi/simi/web/screens/productset/ProductSetEdit.java
+++ simi/modules/web/src/ch/so/agi/simi/web/screens/productset/ProductSetEdit.java
@@ -1,11 +1,40 @@
 package ch.so.agi.simi.web.screens.productset;
 
+import ch.so.agi.simi.entity.SingleActorListProperties;
+import com.haulmont.cuba.gui.components.Button;
+import com.haulmont.cuba.gui.components.Table;
+import com.haulmont.cuba.gui.model.CollectionPropertyContainer;
+import com.haulmont.cuba.gui.model.DataContext;
 import com.haulmont.cuba.gui.screen.*;
 import ch.so.agi.simi.entity.ProductSet;
+
+import javax.inject.Inject;
 
 @UiController("simi_ProductSet.edit")
 @UiDescriptor("product-set-edit.xml")
 @EditedEntityContainer("productSetDc")
 @LoadDataBeforeShow
 public class ProductSetEdit extends StandardEditor<ProductSet> {
+
+    @Inject
+    private Table<SingleActorListProperties> singleActorListTable;
+    @Inject
+    private CollectionPropertyContainer<SingleActorListProperties> singleActorListDc;
+
+    @Subscribe(target = Target.DATA_CONTEXT)
+    public void onPreCommit(DataContext.PreCommitEvent event) {
+        int i = 0;
+        // go through the data container items. The same can be done using getEditedEntity().getSingleActorList().
+        for (SingleActorListProperties item : singleActorListDc.getItems()) {
+            // set new value and add modified instance to the commit list
+            item.setSort(i);
+            event.getModifiedInstances().add(item);
+            i += 10;
+        }
+    }
+
+    @Subscribe("btnSortTable")
+    public void onBtnSortTableClick(Button.ClickEvent event) {
+        singleActorListTable.sort("sort", Table.SortDirection.ASCENDING);
+    }
 }
